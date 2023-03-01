1529e903a1a89cddc9e597c6fe16b0001d38f23b
Tomas Sargiotto <tomas.sargiotto@mastermetrics.com>
2023-03-01 12:14:08 +0000

    feature(transactions): separate transactions logic in controller and router

--- routes/transactionsRoute.js
+++ routes/transactionsRoute.js
@@ -1,59 +1,13 @@
 const express = require("express");
-const Transaction = require("../models/Transaction");
+const TransactionController = require("../controllers/transactions");
 const router = express.Router();
-const moment = require("moment");
-router.post("/add-transaction", async function (req, res) {
-  try {
-    const newtransaction = new Transaction(req.body);
-    await newtransaction.save();
-    res.send("Transaction Added Successfully");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
 
-router.post("/edit-transaction", async function (req, res) {
-  try {
-    await Transaction.findOneAndUpdate({_id : req.body.transactionId} , req.body.payload)
-    res.send("Transaction Updated Successfully");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.post("/oranizations/:organizationId/transactions", TransactionController.createTransaction);
 
-router.post("/delete-transaction", async function (req, res) {
-  try {
-    await Transaction.findOneAndDelete({_id : req.body.transactionId})
-    res.send("Transaction Updated Successfully");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.patch("/oranizations/:organizationId/transactions/:id", TransactionController.updateTransaction);
 
-router.post("/get-all-transactions", async (req, res) => {
-  const { frequency, selectedRange , type } = req.body;
-  try {
-    const transactions = await Transaction.find({
-      ...(frequency !== "custom"
-        ? {
-            date: {
-              $gt: moment().subtract(Number(req.body.frequency), "d").toDate(),
-            },
-          }
-        : {
-            date: {
-              $gte: selectedRange[0],
-              $lte: selectedRange[1],
-            },
-          }),
-      organization: req.body.organizationId,
-      ...(type!=='all' && {type})
-    });
+router.delete("/oranizations/:organizationId/transactions/:id", TransactionController.removeTransaction);
 
-    res.send(transactions);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/oranizations/:organizationId/transactions/", TransactionController.getTransactions );
 
 module.exports = router;
