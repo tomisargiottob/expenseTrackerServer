7704178353da4655d10864993494ad0c1758e46f
Tomas Sargiotto <tomas.sargiotto@mastermetrics.com>
2023-03-03 14:23:32 +0000

    feature(categories): add categories controller methods

--- controllers/categories.js
+++ controllers/categories.js
@@ -4,35 +4,55 @@ const Category = require('../models/Category');
 class CategoriesController {
     static async createCategory (req, res) {
         try {
-          const {id} = req.params
-          const organization = await Organization.findById(id)
+          const {organizationId} = req.params
+          const organization = await Organization.findById(organizationId)
           if(!organization) {
-            res.status(404).json("Organization does not exist");
+            return res.status(404).json("Organization does not exist");
           }
           const {
             name,
+            type, 
+            description
           } = req.body
-          const category = new Category({name, organization});
+          const category = new Category({name, organization: organizationId, type, description});
           await category.save();
           res.send('Organization category successfully added');
         } catch (error) {
-          res.status(500).json(error);
+          res.status(500).json(error.message);
         }
       }
     static async getCategories (req, res) {
         try {
-          const {id} = req.params
-          const organization = await Organization.findById(id)
+          const {organizationId} = req.params
+          const organization = await Organization.findById(organizationId)
           if(!organization) {
-            res.status(404).json("Organization does not exist");
+            return res.status(404).json("Organization does not exist");
           }
-          const categories = await Category.find({organization: id})
+          const categories = await Category.find({organization: organizationId})
           res.send(categories);
         } catch (error) {
           console.log(error)
           res.status(500).json(error);
         }
       }
+    static async removeCategory (req, res) {
+      try {
+        const {organizationId, id} = req.params
+        const organization = await Organization.findById(organizationId)
+        if(!organization) {
+          return res.status(404).json("Organization does not exist");
+        }
+        const category = await Category.find({organization: organizationId, _id:id})
+        if(!category) {
+          return res.status(404).json("Category does not exist");
+        }
+        await Category.deleteOne({organization: organizationId, _id:id})
+        res.status(204).send();
+      } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+      }
+    }
 }
 
 module.exports = CategoriesController
