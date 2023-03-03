a7db2c0bf1fcab74b4e1d53afac12ab072052cc3
Tomas Sargiotto <tomas.sargiotto@mastermetrics.com>
2023-03-03 14:24:13 +0000

    feature(category): add properties to category schema

--- models/Category.js
+++ models/Category.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
   organization : { type: String, required: true, ref: 'Organization'},
   name: { type: String, required: true },
+  type: { type: String, required: true },
+  description: { type: String, required: true },
 });
 
 const categoryModel = mongoose.model("Categories", categorySchema);
