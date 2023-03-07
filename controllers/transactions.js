9a961e03abd1f86ba2514adc8b5b826a42990c44
Tomas Sargiotto <tomas.sargiotto@mastermetrics.com>
2023-03-07 00:22:15 +0000

    feature(transactions): populate data before sending

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -22,37 +22,37 @@ class TransactionController {
             }),
         organization: req.params.organizationId,
         ...(type!=='all' && {type})
-      });
-  
+      }).populate(['account','category']);
+
       res.send(transactions);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  }
+  static async removeTransaction (req, res) {
+    try {
+      await Transaction.findOneAndDelete({_id : req.params.id, organization: req.params.organizationId})
+      res.send("Transaction Successfully Removed");
     } catch (error) {
       res.status(500).json(error);
     }
   }
-  static async removeTransaction (req, res) {
-      try {
-        await Transaction.findOneAndDelete({_id : req.params.id, organization: req.params.organizationId})
-        res.send("Transaction Successfully Removed");
-      } catch (error) {
-        res.status(500).json(error);
-      }
-  }
   static async updateTransaction (req, res) {
-      try {
-        await Transaction.findOneAndUpdate({_id : req.params.id, organization: req.params.organizationId} , req.body)
-        res.send("Transaction Updated Successfully");
-      } catch (error) {
-        res.status(500).json(error);
-      }
+    try {
+      await Transaction.findOneAndUpdate({_id : req.params.id, organization: req.params.organizationId} , req.body)
+      res.send("Transaction Updated Successfully");
+    } catch (error) {
+      res.status(500).json(error);
     }
+  }
   static async createTransaction (req, res) {
-      try {
-          const newtransaction = new Transaction(req.body);
-          await newtransaction.save();
-          res.send("Transaction Added Successfully");
-      } catch (error) {
-          res.status(500).json(error);
-      }
+    try {
+      const newtransaction = new Transaction(req.body);
+      await newtransaction.save();
+      res.send("Transaction Added Successfully");
+    } catch (error) {
+      res.status(500).json(error);
+    }
   }
 }
 
