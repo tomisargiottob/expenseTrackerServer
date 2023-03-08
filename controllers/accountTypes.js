83bca4c1bcd9541f95d5eb6371723077068f4838
Tomas Sargiotto <tomas.sargiotto@mastermetrics.com>
2023-03-08 22:57:04 +0000

    feature(accountType): add account Type controller

--- controllers/accountTypes.js
+++ controllers/accountTypes.js
@@ -1,10 +1,10 @@
 const Organization = require('../models/Organization');
-const Account = require('../models/Account');
+const AccountType = require('../models/AccountType');
 const Transaction = require('../models/Transaction');
 
 
-class AccountsController {
-    static async createAccount (req, res) {
+class AccountTypesController {
+    static async createAccountType (req, res) {
       try {
         const {organizationId} = req.params
         const organization = await Organization.findById(organizationId)
@@ -14,60 +14,60 @@ class AccountsController {
         const {
           name,
         } = req.body
-        const existentAccount = await Account.find({name,organization: organizationId})
+        const existentAccount = await AccountType.find({name,organization: organizationId})
         if(existentAccount.length) throw new Error('Account already exists in organization')
-        const account = new Account({name, organization: organizationId});
+        const account = new AccountType({name, organization: organizationId});
         await account.save();
-        res.send('Organization Account successfully added');
+        res.send('Organization Account type successfully added');
       } catch (error) {
         console.log(error.message)
         res.status(500).json(error.message);
       }
     }
-    static async getAccounts (req, res) {
+    static async getAccountTypes (req, res) {
       try {
         const {organizationId} = req.params
         const organization = await Organization.findById(organizationId)
         if(!organization) {
           return res.status(404).json("Organization does not exist");
         }
-        const accounts = await Account.find({organization: organizationId})
-        res.send(accounts);
+        const accountTypes = await AccountType.find({organization: organizationId})
+        res.send(accountTypes);
       } catch (error) {
         console.log(error)
         res.status(500).json(error);
       }
     }
-    static async removeAccount(req, res) {
+    static async removeAccountType(req, res) {
       try {
         const {organizationId, id} = req.params
         const organization = await Organization.findById(organizationId)
         if(!organization) {
           return res.status(404).json("Organization does not exist");
         }
-        const account = await Account.find({organization: organizationId, _id:id})
-        if(!account) {
-          return res.status(404).json("Account does not exist");
+        const accountType = await AccountType.find({organization: organizationId, _id:id})
+        if(!accountType) {
+          return res.status(404).json("Account type does not exist");
         }
         const transactions = await Transaction.count({account: id})
         if(transactions) {
           return res.status(400).json(`Can not delete an account that has ${transactions} transactions created`)
         }
-        await Account.deleteOne({organization: organizationId, _id:id})
+        await AccountType.deleteOne({organization: organizationId, _id:id})
         res.status(204).send();
       } catch (error) {
         console.log(error)
         res.status(500).json(error);
       }
     }
-    static async updateAccount (req, res) {
+    static async updateAccountType (req, res) {
       try {
-        await Account.findOneAndUpdate({_id : req.params.id, organization: req.params.organizationId} , req.body)
-        res.send("Account Updated Successfully");
+        await AccountType.findOneAndUpdate({_id : req.params.id, organization: req.params.organizationId} , req.body)
+        res.send("Account type Updated Successfully");
       } catch (error) {
         res.status(500).json(error);
       }
     }
 }
 
-module.exports = AccountsController
+module.exports = AccountTypesController
