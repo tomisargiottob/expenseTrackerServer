49f73addca5b6ef61f3a8769092e1340961fba55
Tomas Sargiotto <tomas.sargiotto@mastermetrics.com>
2023-05-17 17:08:46 -0300

    chore(db): allow develop database connection

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -3,10 +3,18 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-mongoose.connect(
-  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gifny.mongodb.net/spending`,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+if (process.env.DATABASE_ENV === 'develop') {
+  mongoose.connect(
+    `${process.env.DB_DEVELOP_URI}`, {
+      dbName: 'spending'
+    }
+  );
+} else {
+  mongoose.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gifny.mongodb.net/spending`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+}
 
 
 const connection = mongoose.connection
