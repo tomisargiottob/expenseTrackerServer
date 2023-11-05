1199a2cec0a7beb02c63b91c49daff749f2497d6
Tomas Sargiotto <tomas.sargiotto@mastermetrics.com>
2023-11-05 16:19:59 -0300

    update ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
     apps : [{
       name: "My App",
-      script: "src/server.js",
-      instances: "max",
+      script: "npm run start",
+      instances: "1",
       max_memory_restart: "256M",
       env: {
         NODE_ENV: "development"
       },
       env_production: {
         NODE_ENV: "production"
-      }
+      },
     }]
 }
